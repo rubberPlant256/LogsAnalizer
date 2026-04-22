24bc2554adfc15b57032021cb75844a8fc765c67
rubberPlant256 <ihrustal@yandex.ru>
2026-04-22 16:22:32 +0300

    init

---
+++ ingestion-service/src/test/java/org/strongcat/IngestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.strongcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngestionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
