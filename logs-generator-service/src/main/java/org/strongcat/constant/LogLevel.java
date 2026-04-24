2893778aa9ca7a9791ee9fbc811e43be13d22492
rubberPlant256 <ihrustal@yandex.ru>
2026-04-24 13:11:54 +0300

    logging in console + docker-compose for kafka

---
+++ logs-generator-service/src/main/java/org/strongcat/constant/LogLevel.java
@@ -0,0 +1,13 @@
+package org.strongcat.constant;
+
+import java.util.Random;
+
+public enum LogLevel {
+    INFO, DEBUG, WARN, ERROR, TRACE;
+
+    private static final Random RANDOM = new Random();
+
+    public static LogLevel getRandom() {
+        return values()[RANDOM.nextInt(values().length)];
+    }
+}
---
+++ logs-generator-service/src/main/java/org/strongcat/service/LogGeneratorService.java
@@ -0,0 +1,42 @@
+package org.strongcat.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.strongcat.constant.LogLevel;
+import org.strongcat.constant.LogMessageData;
+import org.strongcat.data.LogMessage;
+
+import java.util.UUID;
+
+import static net.logstash.logback.argument.StructuredArguments.fields;
+
+@Service
+@Slf4j
+public class LogGeneratorService {
+
+    @Scheduled(fixedRate = 5000)
+    public void generate() {
+        LogLevel level = LogLevel.getRandom();
+        String message = LogMessageData.getRandomMessage();
+
+        LogMessage meta = LogMessage.builder()
+                .id(UUID.randomUUID())
+                .traceId(UUID.randomUUID().toString().substring(0, 8))
+                .build();
+
+        logToConsole(level, message, meta);
+
+        // 4. (Скоро) sendToKafka(level, message, meta);
+    }
+
+    private void logToConsole(LogLevel level, String message, LogMessage meta) {
+        switch (level) {
+            case INFO  -> log.info(message, fields(meta));
+            case ERROR -> log.error(message, fields(meta));
+            case WARN  -> log.warn(message, fields(meta));
+            case DEBUG -> log.debug(message, fields(meta));
+            case TRACE -> log.trace(message, fields(meta));
+        }
+    }
+}
